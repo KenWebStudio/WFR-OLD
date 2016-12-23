d278703c3b1d64398394f14ebdc06c2bfc07644b
KennethDLP <pane.saldes@gmail.com>
2016-12-23 13:55:38 +0800

    Added flickity.

--- js/site-scripts.js
+++ js/site-scripts.js
@@ -1,6 +1,14 @@
+// Initiate Sticky Top Navigation
 $(document).ready(function () {
   $("#nav-sticky").sticky({
     topSpacing:0,
     zIndex: 9999
   });
+})
+
+// Initiate Flickity Carousel for #portfolio
+$('.featured-works-carousel').flickity ({
+  contain: true,
+  groupCells: true,
+  autoPlay: 2500
 })
